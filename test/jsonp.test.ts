7c0c72f198a65eccda79899e5ac28832b1ddb04f
fengmk2 <fengmk2@gmail.com>
2025-01-11 17:32:58 +0800

    feat: support cjs and esm both by tshy
    BREAKING CHANGE: drop Node.js < 18.19.0 support
    
    part of https://github.com/eggjs/egg/issues/3644
    
    https://github.com/eggjs/egg/issues/5257

--- test/jsonp.test.ts
+++ test/jsonp.test.ts
@@ -1,9 +1,7 @@
-'use strict';
+import { mm, MockApplication } from '@eggjs/mock';
 
-const mm = require('egg-mock');
-
-describe('test/jsonp.test.js', () => {
-  let app;
+describe('test/jsonp.test.ts', () => {
+  let app: MockApplication;
   before(() => {
     app = mm.app({
       baseDir: 'jsonp-test',
@@ -14,56 +12,56 @@ describe('test/jsonp.test.js', () => {
   after(() => app.close());
   afterEach(mm.restore);
 
-  it('should support json', function* () {
-    yield app.httpRequest()
+  it('should support json', async () => {
+    await app.httpRequest()
       .get('/default')
       .expect(200)
       .expect({ foo: 'bar' });
   });
 
-  it('should support jsonp', function* () {
-    yield app.httpRequest()
+  it('should support jsonp', async () => {
+    await app.httpRequest()
       .get('/default?callback=fn')
       .expect(200)
       .expect('/**/ typeof fn === \'function\' && fn({"foo":"bar"});');
   });
 
-  it('should support _callback', function* () {
-    yield app.httpRequest()
+  it('should support _callback', async () => {
+    await app.httpRequest()
       .get('/default?_callback=fn')
       .expect(200)
       .expect('/**/ typeof fn === \'function\' && fn({"foo":"bar"});');
   });
 
-  it('should support jsonp if response is empty', function* () {
-    yield app.httpRequest()
+  it('should support jsonp if response is empty', async () => {
+    await app.httpRequest()
       .get('/empty?callback=fn')
       .expect(200)
       .expect('/**/ typeof fn === \'function\' && fn(null);');
   });
 
-  it('should not support jsonp if not use jsonp middleware', function* () {
-    yield app.httpRequest()
+  it('should not support jsonp if not use jsonp middleware', async () => {
+    await app.httpRequest()
       .get('/disable?_callback=fn')
       .expect(200)
       .expect({ foo: 'bar' });
   });
 
-  it('should not support cutom callback name', function* () {
-    yield app.httpRequest()
+  it('should not support custom callback name', async () => {
+    await app.httpRequest()
       .get('/fn?fn=fn')
       .expect(200)
       .expect('/**/ typeof fn === \'function\' && fn({"foo":"bar"});');
   });
 
-  it('should not pass csrf', function* () {
-    yield app.httpRequest()
+  it('should not pass csrf', async () => {
+    await app.httpRequest()
       .get('/csrf')
       .expect(403);
   });
 
-  it('should pass csrf with cookie', function* () {
-    yield app.httpRequest()
+  it('should pass csrf with cookie', async () => {
+    await app.httpRequest()
       .get('/csrf')
       .set('cookie', 'csrfToken=token;')
       .set('x-csrf-token', 'token')
@@ -71,8 +69,8 @@ describe('test/jsonp.test.js', () => {
       .expect({ foo: 'bar' });
   });
 
-  it('should pass csrf with cookie and support jsonp', function* () {
-    yield app.httpRequest()
+  it('should pass csrf with cookie and support jsonp', async () => {
+    await app.httpRequest()
       .get('/csrf')
       .set('cookie', 'csrfToken=token;')
       .set('x-csrf-token', 'token')
@@ -80,86 +78,86 @@ describe('test/jsonp.test.js', () => {
       .expect({ foo: 'bar' });
   });
 
-  it('should pass referrer white list check with subdomain', function* () {
-    yield app.httpRequest()
+  it('should pass referrer white list check with subdomain', async () => {
+    await app.httpRequest()
       .get('/referrer/subdomain')
       .set('referrer', 'http://test.com/')
       .expect(200)
       .expect({ foo: 'bar' });
 
-    yield app.httpRequest()
+    await app.httpRequest()
       .get('/referrer/subdomain')
       .set('referrer', 'http://sub.test.com/')
       .expect(200)
       .expect({ foo: 'bar' });
 
-    yield app.httpRequest()
+    await app.httpRequest()
       .get('/referrer/subdomain')
       .set('referrer', 'https://sub.sub.test.com/')
       .expect(200)
       .expect({ foo: 'bar' });
 
-    yield app.httpRequest()
+    await app.httpRequest()
       .get('/referrer/subdomain')
       .set('referrer', 'https://sub.sub.test1.com/')
       .expect(403)
       .expect(/jsonp request security validate failed/);
   });
 
-  it('should pass referrer white list with domain', function* () {
-    yield app.httpRequest()
+  it('should pass referrer white list with domain', async () => {
+    await app.httpRequest()
       .get('/referrer/equal')
       .set('referrer', 'http://test.com/')
       .expect(200)
       .expect({ foo: 'bar' });
 
-    yield app.httpRequest()
+    await app.httpRequest()
       .get('/referrer/equal')
       .set('referrer', 'https://test.com/')
       .expect(200)
       .expect({ foo: 'bar' });
 
-    yield app.httpRequest()
+    await app.httpRequest()
       .get('/referrer/equal')
       .set('referrer', 'https://sub.sub.test.com/')
       .expect(403)
       .expect(/jsonp request security validate failed/);
 
-    yield app.httpRequest()
+    await app.httpRequest()
       .get('/referrer/equal')
       .set('referrer', 'https://sub.sub.test1.com/')
       .expect(403)
       .expect(/jsonp request security validate failed/);
   });
 
-  it('should pass referrer white array and regexp', function* () {
-    yield app.httpRequest()
+  it('should pass referrer white array and regexp', async () => {
+    await app.httpRequest()
       .get('/referrer/regexp')
       .set('referrer', 'http://test.com/')
       .expect(200)
       .expect({ foo: 'bar' });
 
-    yield app.httpRequest()
+    await app.httpRequest()
       .get('/referrer/regexp')
       .set('referrer', 'https://foo.com/')
       .expect(200)
       .expect({ foo: 'bar' });
 
-    yield app.httpRequest()
+    await app.httpRequest()
       .get('/referrer/regexp')
       .set('referrer', 'https://sub.sub.test.com/')
       .expect(403)
       .expect(/jsonp request security validate failed/);
 
-    yield app.httpRequest()
+    await app.httpRequest()
       .get('/referrer/regexp')
       .set('referrer', 'https://sub.sub.test1.com/')
       .expect(403)
       .expect(/jsonp request security validate failed/);
   });
 
-  it('should pass when pass csrf but not hit referrer white list', function* () {
-    yield app.httpRequest()
+  it('should pass when pass csrf but not hit referrer white list', async () => {
+    await app.httpRequest()
       .get('/both')
       .set('cookie', 'csrfToken=token;')
       .set('x-csrf-token', 'token')
@@ -167,45 +165,45 @@ describe('test/jsonp.test.js', () => {
       .expect({ foo: 'bar' });
   });
 
-  it('should pass when not pass csrf but hit referrer white list', function* () {
-    yield app.httpRequest()
+  it('should pass when not pass csrf but hit referrer white list', async () => {
+    await app.httpRequest()
       .get('/both')
       .set('referrer', 'https://test.com/')
       .expect(200)
       .expect({ foo: 'bar' });
   });
 
-  it('should 403 when not pass csrf and not hit referrer white list', function* () {
-    yield app.httpRequest()
+  it('should 403 when not pass csrf and not hit referrer white list', async () => {
+    await app.httpRequest()
       .get('/both')
       .expect(403)
       .expect(/jsonp request security validate failed/);
   });
 
-  it('should 403 when not pass csrf and referrer illegal', function* () {
-    yield app.httpRequest()
+  it('should 403 when not pass csrf and referrer illegal', async () => {
+    await app.httpRequest()
       .get('/both')
       .set('referrer', '/hello')
       .expect(403)
       .expect(/jsonp request security validate failed/);
   });
 
-  it('should pass and return is a jsonp function', function* () {
-    yield app.httpRequest()
+  it('should pass and return is a jsonp function', async () => {
+    await app.httpRequest()
       .get('/mark?_callback=fn')
       .expect(200)
       .expect('/**/ typeof fn === \'function\' && fn({"jsonpFunction":true});');
   });
 
-  it('should pass and return is not a jsonp function', function* () {
-    yield app.httpRequest()
+  it('should pass and return is not a jsonp function', async () => {
+    await app.httpRequest()
       .get('/mark')
       .expect(200)
       .expect({ jsonpFunction: false });
   });
 
-  it('should pass and return error message', function* () {
-    yield app.httpRequest()
+  it('should pass and return error message', async () => {
+    await app.httpRequest()
       .get('/error?_callback=fn')
       .expect(200)
       .expect('/**/ typeof fn === \'function\' && fn({"msg":"jsonpFunction is error"});');
